b6c428acc41b71d7e93d57f1d8768aae9c3404f9
Max <bespalov.m.9@ya.ru>
2022-12-22 19:52:44 +0300

    initial commit, added 2 java classes

---
+++ src/main/java/Console.java
@@ -0,0 +1,12 @@
+public class Console {
+
+    public void print(String text) {
+        System.out.println(text);
+    }
+
+    public void print(int text) {
+        System.out.println(text);
+    }
+
+
+}
