6b9c81c62c0e399fcc1f4438d48ef279e8eecc2f
Max <bespalov.m.9@ya.ru>
2022-12-23 16:29:02 +0300

    Added rubles in result (Calculator.class), refactoring

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
     private int numberOfPeople;
     private double costSum;
     private List<Item> items = new ArrayList<>();
+    private StringBuilder result = new StringBuilder("Добавленные товары:");
 
 
     public Calculator(int numberOfPeople) {
@@ -13,17 +14,45 @@ public class Calculator {
     }
 
     public String calculate() {
-        StringBuilder result = new StringBuilder("Добавленные товары:");
+
+        String costForEachPersonString = (String.format("%.2f", costSum/numberOfPeople));
         for (Item item : items) result.append(System.lineSeparator())
+                                      .append("   ")
+                                      .append(items.indexOf(item) + 1)
+                                      .append(".  ")
                                       .append(item.getName());
         result.append(System.lineSeparator())
                 .append("С каждого человека по ")
-                .append(String.format("%.2f",costSum/numberOfPeople));
+                .append(costForEachPersonString);
+
+        addRublesToResult(costForEachPersonString);
+
         return result.toString();
     }
 
     public void addItem(Item item){
         items.add(item);
         costSum+=item.getPrice();
+    }
+
+    public void addRublesToResult(String costForEachPersonString) {
+        switch (costForEachPersonString.charAt(costForEachPersonString.length() - 4)){
+            case '1':
+                result.append(" рубль");
+                break;
+            case '2':
+            case '3':
+            case '4':
+                result.append(" рубля");
+                break;
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '0':
+                result.append(" рублей");
+                break;
+        }
     }
 }
