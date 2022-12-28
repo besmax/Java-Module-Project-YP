f81e89b7939105f68287c840b3a4fbc5bd16f814
Max <bespalov.m.9@ya.ru>
2022-12-28 18:49:08 +0300

    change addRublesToResult() in Calculator.class

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.pow;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,7 +28,7 @@ public class Calculator {
                 .append("С каждого человека по ")
                 .append(costForEachPersonString);
 
-        addRublesToResult(costSum / numberOfPeople);
+        result.append(addRublesToResult(costSum / numberOfPeople));
 
         return result.toString();
     }
@@ -38,44 +40,27 @@ public class Calculator {
         }
     }
 
-    public void addRublesToResult(double costForEachPerson) {
-        int costForEachPersonInt = (int) costForEachPerson;
-        switch (Integer.toString(costForEachPersonInt).substring(Integer.toString(costForEachPersonInt).length() - 2)) {
-            case "01":
-                result.append(" рубль");
-                break;
-            case "02":
-            case "22":
-            case "32":
-            case "42":
-            case "52":
-            case "62":
-            case "72":
-            case "82":
-            case "92":
-            case "03":
-            case "23":
-            case "33":
-            case "43":
-            case "53":
-            case "63":
-            case "73":
-            case "83":
-            case "93":
-            case "04":
-            case "24":
-            case "34":
-            case "44":
-            case "54":
-            case "64":
-            case "74":
-            case "84":
-            case "94":
-                result.append(" рубля");
-                break;
-            default:
-                result.append(" рублей");
-                break;
-        }
+    public String addRublesToResult(double costForEachPerson) {
+        int twoLastDigits = getTwoLastDigits((int) costForEachPerson);
+        if (getTwoLastDigits((int) costForEachPerson) > 10 && getTwoLastDigits((int) costForEachPerson) < 15) return " рублей";
+        else if (getTwoLastDigits((int) costForEachPerson) % 10 == 1) return " рублю";
+        else if (getTwoLastDigits((int) costForEachPerson) % 10 > 1 && getTwoLastDigits((int) costForEachPerson) % 10 < 5) return " рубля";
+        else return " рублей";
+    }
+
+    public int getTwoLastDigits(int number) {
+        int result = number;
+
+        while (result >= 100) result = (int) (result - (pow(10, (getNumberLength(result)) - 1) *
+                trimNumber(result, getNumberLength(result) - 1)));
+        return result;
+    }
+
+    public int getNumberLength(int number) {
+        return (int) (Math.log10(number) + 1);
+    }
+
+    public int trimNumber(int number, int digits) {
+        return (int) (number/ pow(10, digits));
     }
 }
